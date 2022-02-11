60df8aafb316e1a10d81b2027152ab71445f39da
Haziq Naeem <haziqnaeem36@gmail.com>
2022-02-11 11:59:45 -0500

    create game screen

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
-console.log("test")
+/*-------------------------------- Constants --------------------------------*/
+
+
+
+/*---------------------------- Variables (state) ----------------------------*/
+
+
+
+/*------------------------ Cached Element References ------------------------*/
+
+const board = document.getElementById('board')
+console.log(board)
+
+const statusMessage = document.getElementById('message')
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+
+
+/*-------------------------------- Functions --------------------------------*/
+
+
